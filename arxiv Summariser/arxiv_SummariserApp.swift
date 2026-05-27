2e67b2cf815a6db2c55d0e78d6fe1ffc69555822
ravjot-sk <ravjot.kohli@uni-muenster.de>
2026-05-28 00:09:48 +0200

    Initial Commit

---
+++ arxiv Summariser/arxiv_SummariserApp.swift
@@ -0,0 +1,17 @@
+//
+//  arxiv_SummariserApp.swift
+//  arxiv Summariser
+//
+//  Created by Ravjot on 28.05.26.
+//
+
+import SwiftUI
+
+@main
+struct arxiv_SummariserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
